08c5a622459955ea2ceec93095ff4bade8f85eec
ChenGuiFa <545516381@qq.com>
2020-08-20 12:01:18 +0800

    格式化返回时间
    格式化返回时间

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 
@@ -5,4 +6,6 @@ class Base(BaseModel):
 
     class Config:
         orm_mode = True
-
+        json_encoders = {
+            datetime: lambda v: v.strftime('%Y-%m-%d %H:%M:%S')
+        }
