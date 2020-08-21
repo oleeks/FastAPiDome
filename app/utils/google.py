eab99d26864eb697277c0d22eb13d8a36ea8f990
ChenGuiFa <545516381@qq.com>
2020-08-21 16:39:33 +0800

    add Comment

--- app/utils/google.py
+++ app/utils/google.py
@@ -7,9 +7,11 @@ def generate_secret_key() -> str:
     
     return pyotp.random_base32()
 
-def verify_code(secret_key, verify_code):
+def verify_code(secret_key, verify_code) -> bool:
     """
     Verify that the google code is correct
+    :param secret_key: google secret key
+    :param verify_code: google authernticator code
     """
     t = pyotp.TOTP(secret_key)
     return t.verify(verify_code)
